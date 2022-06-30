3a613be699bfd420c865fd067ae9a72a50ef8aa7
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-06-30 16:48:50 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=tuyo
-export POSTGRES_PASSWORD=Adedolapo1$
-export POSTGRES_HOST=tuyo-udagram.cc75twpkniee.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=YOUR_USERNAME
+export POSTGRES_PASSWORD=YOUR_PASSWORD
+export POSTGRES_HOST=udagram.cc75tafdsfade.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-673279660867-bucket
+export AWS_BUCKET=arn:aws:s3:::bucket-1234567890-name
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=itWorks
