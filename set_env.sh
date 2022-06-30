d217b05100563ebb0891a1aff50c720ee73caa52
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-06-30 15:30:50 +0100

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=tuyo
+export POSTGRES_PASSWORD=Adedolapo1$
+export POSTGRES_HOST=tuyo-udagram.cc75twpkniee.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-673279660867-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=itWorks
+export URL=http://localhost:8100
