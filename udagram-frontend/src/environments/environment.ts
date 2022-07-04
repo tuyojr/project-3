4ab8f0d3179e1ba5a56c32682bfde7e305b227a3
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-07-04 12:03:41 +0100

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abc54d64bfcce414b9cdbc2de6890a28-861209571.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
